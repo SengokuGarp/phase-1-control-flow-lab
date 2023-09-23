b854d01fea6d6043591c8f9fa8fff633028871e3
Glen <garpirungu@gmail.com>
2023-09-23 09:41:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet) {
   // Write your code here!
+  if (distanceInFeet <= 400) {
+    return "This one is on me!";
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination) {
   // Write your code here!
+  return destination === "Nairobi" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
